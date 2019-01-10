0bc8642a8bebd46ed973ad7f9232c8ee10740227
LST-LM <1548861368@qq.com>
2019-01-10 10:02:00 +0800

    123

---
+++ 1.php
@@ -0,0 +1,9 @@
+<?php
+header("Access-Control-Allow-Origin:http://10.30.29.73:63341");
+header("Access-Control-Allow-Credentials:true");
+header('Access-Control-Allow-Headers:Content-Type');
+
+$array=array("userName"=>"111111111");
+echo json_encode($array);
+
+?>
